fa1b2bc73f56b9fdbf8d4a52378e6a89acc1faae
tenisistaa <krzysztofogaza@gmail.com>
2017-12-21 22:33:56 +0100

    Creating files and folders

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+//Konfiguracja Webpack
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    entry: ["./src/jsx/index.jsx","./src/scss/style.scss"],
+    output: { filename: "./dist/js/out.js" },
+    devServer: {
+        inline: true,
+        contentBase: './',
+        port: 3001
+    },
+    watch: true,
+
+    module: {
+        loaders: [
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: { presets: ['es2015', 'stage-2', 'react'] }
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'sass-loader']
+                })
+
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif)$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {}
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('./dist/css/style.css')
+
+    ]
+};
